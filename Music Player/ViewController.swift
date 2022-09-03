194f04e58bd383d38cb857a1da2f2872118c5799
MrSalei <ilia74311@yandex.by>
2022-09-03 10:18:24 +0300

    Initial Commit

---
+++ Music Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Player
+//
+//  Created by luqrri on 3.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
